d1b3cd79ad4b2a089d5c5dbac38ffd251d0e5b9e
Dario <zalovichdario@gmail.com>
2025-11-19 21:07:24 -0300

    cambio titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header>
-      <h1 className="text-4xl font-semibold">Discover</h1>
+      <h1 className="text-4xl font-semibold">Discover Posts</h1>
       <p className="text-xs text-gray-500">News from all around the world</p>
     </header>
   );
