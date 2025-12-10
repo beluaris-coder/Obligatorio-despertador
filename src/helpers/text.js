2bb9c6ab004014b5070e739a6a791b052129f6db
solalexandrovic <sol.alexandrovic@gmail.com>
2025-12-10 19:05:06 -0300

    sol text

--- src/helpers/text.js
+++ src/helpers/text.js
@@ -1,2 +1,9 @@
 export const capitalizar = (texto) =>
   texto ? texto.charAt(0).toUpperCase() + texto.slice(1) : texto;
+
+export const labelFuncionalidad = (texto) => {
+  if (!texto) return "";
+  return texto
+    .replace(/_/g, " ")      // convierte guiones bajos a espacios
+    .replace(/\b\w/g, (l) => l.toUpperCase());  // pone cada palabra con mayúscula
+};
